bc274e56b15886d46896ccaf7762564fa12adbda
oluwaseun anthony <oluwaseun.a.anthony@gmail.com>
2023-10-18 11:02:23 -0500

    add the first test and solution for intro-to-test

--- code.js
+++ code.js
@@ -2,3 +2,11 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function isSeven(input){
+    return input==7
+}
+
+function sayHello(){
+    return "";
+}
--- tests.js
+++ tests.js
@@ -13,3 +13,30 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('isSeven',function(){
+    it('should be defined as a function', () => {
+        expect(typeof isSeven).toBe('function')
+    });
+    it('should return a number data type',function()  {
+        expect(typeof isSeven()).toBe('boolean')
+    });
+    it('should return false if passed the number 5', () => {
+        expect(isSeven(5)).toBe(false);
+    });
+    it('should return true if passed the number 7', () => {
+        expect(isSeven(7)).toBe(true);
+    });
+    it('should return true if passed "7', () => {
+        expect(isSeven('7')).toBe(true)
+    });
+})
+
+describe('sayHello',function(){
+    it('should be defined as a function', () => {
+        expect(typeof sayHello).toBe('function')
+    });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+})
